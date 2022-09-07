5ad80df2f542caa363d5129cb1b001f7c728fbe1
Selim <mamo31st@gmail.com>
2022-09-08 00:00:19 +0200

    added a server.ts file to the root project

--- app.ts
+++ app.ts
@@ -2,11 +2,12 @@ import express, { Application } from 'express';
 import { config } from './src/config/config';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
-import routes from './src/app';
+import routes from './src/server';
+import path from 'path';
 
 const app: Application = express();
 const port = config.PORT || 8000;
-const localhost = `http://localhost:${port}/`;
+const localhost = `http://localhost:${port}/login`;
 
 app.use(bodyParser.urlencoded({ 
     extended: false
@@ -16,6 +17,8 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.use(helmet());
+
+app.use(express.static(path.normalize(path.resolve('./public'))));
 
 app.listen(port, () => {
     console.log(`server is running on: ${localhost}`);
